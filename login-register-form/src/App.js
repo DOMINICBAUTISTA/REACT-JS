4f44eb9987d2d069f13dd2747a7d29f6d7b29ab9
DOMINICBAUTISTA <117502327+DOMINICBAUTISTA@users.noreply.github.com>
2023-02-09 21:48:38 +0800

    Update App.js

--- login-register-form/src/App.js
+++ login-register-form/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { Login } from "./Login";
-import { Register } from "./Register";
+import Register from "./Register";
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
